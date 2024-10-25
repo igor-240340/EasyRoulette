3f6dc41baf9c2e675c632f562a017e2cdcd8166c
igor <igor.240340@gmail.com>
2024-10-25 13:45:16 +0700

    Integrate with YandexGames

--- assets/Scripts/RewardButtonHandler.ts
+++ assets/Scripts/RewardButtonHandler.ts
@@ -1,18 +1,28 @@
 import { _decorator, Component, Node } from 'cc';
+import { ysdk } from 'db://yandex-games-sdk/ysdk';
+
 const { ccclass, property } = _decorator;
 
 @ccclass('RewardButtonHandler')
 export class RewardButtonHandler extends Component {
     start() {
-
     }
 
     update(deltaTime: number) {
-        
     }
 
     onButtonClick() {
-        console.log('Button was clicked!');
+        console.log('Reward button was clicked!');
+
+        const callbacks = {
+            onRewarded: this.onReward.bind(this),
+        };
+
+        ysdk.adv.showRewardedVideo({ callbacks });
+    }
+
+    onReward() {
+        console.log('onReward');
     }
 }
 
